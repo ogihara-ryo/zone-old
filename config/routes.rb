565d1a21038a00625e752df7804803f4ed8c77e8
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2017-01-02 18:56:50 +0900

    Narrow task routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   delete 'signout' => 'sessions#destroy'
 
   resources :users, only: [] do
-    resources :tasks do
+    resources :tasks, only: %i(index create update destroy) do
       put 'start'
       put 'finish'
     end
