89947ae3fcbf8c58ea732c8d9da2b2d0eee64c35
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2017-01-02 19:43:41 +0900

    Fix migration

--- db/migrate/20170102061455_create_tasks.rb
+++ db/migrate/20170102061455_create_tasks.rb
@@ -1,10 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
+      t.date :when
       t.string :title
       t.integer :estimate
       t.integer :actual
-      t.integer :status
+      t.integer :status, default: 0, null: false
       t.belongs_to :user, foreign_key: true
 
       t.timestamps
