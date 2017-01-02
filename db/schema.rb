3ab98ae6111a3e3df8bdc2e66ec06a6c3462e734
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2017-01-02 19:46:27 +0900

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,14 @@ ActiveRecord::Schema.define(version: 20170102061455) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
+    t.date     "when"
     t.string   "title"
     t.integer  "estimate"
     t.integer  "actual"
-    t.integer  "status"
+    t.integer  "status",     default: 0, null: false
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
